b94c72ae11468e4fed59e9ad8d13c4e26d43218c
Narangerel0121 <nrngrl0121@gmail.com>
2025-02-05 12:46:18 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,65 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // const tasks = [
+  //   {
+  //     text: "geree tseverleh",
+  //     isCompleted: false,
+  //     id: new Date().toISOString()
+  //   },
+  //   {
+  //     text: "hooloo tseverleh",
+  //     isCompleted: false,
+  //     id: new Date().toISOString()
+  //   }
+  // ]
+  // {console.log(tasks[1].id)} // ajilj bainaa
+  const initialTasks = [];
+  const [tasks, setTasks] = useState(initialTasks);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  function addTask() {
+    const newTaskText = prompt("task?")
+
+    if (newTaskText) {
+      const newTasks = [{ text: newTaskText, isCompleted: false, id: nanoid() }, ...tasks];
+      setTasks(newTasks)
+    }
+  }
+
+  function editTask(index) {
+    const updatedTaskText = prompt("Edit?", tasks[index].text);
+
+    if (updatedTaskText) {
+      const newTasks = [...tasks];
+      newTasks[index].text = updatedTaskText;
+      setTasks(newTasks);
+    }
+  }
+
+  function deleteTask(id) {
+    if(confirm("U sure?")) {
+      const newTasks = tasks.filter((task) => task.id !== id);
+      setTasks(newTasks)
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={addTask} >Add</button>
+      {tasks.map((task, index) => (
+        <div key={task.id} className={task}>
+
+          <input type="checkbox"></input>
+
+          <span>{task.text}</span>
+
+          <button onClick={() => editTask(index)}>Edit</button>
+          <button onClick={() => deleteTask(task.id)}>Delete</button>
+
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ))}
     </div>
   );
 }
